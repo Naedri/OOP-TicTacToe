98134ed74f532d09f170ed933749bd8686915690
Naedri <adrien.jallais@protonmail.com>
2019-12-28 12:46:33 +0100

    appli1 ok

--- OOP-TicTacToe/src/oop/tictactoe/appli/PartieTicTacToe.java
+++ OOP-TicTacToe/src/oop/tictactoe/appli/PartieTicTacToe.java
@@ -1,6 +1,5 @@
 package oop.tictactoe.appli;
 
-import oop.tictactoe.mouvements.MouvementsPermutation;
 import oop.tictactoe.mouvements.MouvementsTicTacToe;
 import oop.tictactoe.jouer.*;
 import oop.tictactoe.grille.*;
@@ -8,7 +7,6 @@ import oop.tictactoe.grille.*;
 
 public class PartieTicTacToe {
 	
-	private Messages interf ;	
 	private Joueur joueur1 ;
 	private Joueur joueur2 ;
 	private Match match ;
@@ -19,24 +17,33 @@ public class PartieTicTacToe {
 		joueur2 = new Joueur();
 		grille = new Grille();
 		match = new Match();
-		interf = new Messages();
 	}
-	//ATTENTION PEUT ETRE PASSER DE STRING A VOID POUR GET MESSAGE POUR EVITER REPEPTITOIN DE SYSO
 	public void lancerPartie() {
 		grille.afficherGrille();
 		while(!(match.estTermine() || match.estVictoire())) {
 			match.tourDebut();
 			Joueur joueurActuel = ( match.getTour()%2 == 0 ) ? joueur2 : joueur1 ;
-			interf.afficherMessageTour(joueurActuel);
-			interf.setSaisieCellule(grille);
-			interf.afficherMessageCellule(joueurActuel);
-			grille.placerJeton(joueurActuel.getJeton(), interf.getSaisieLigne(), interf.getSaisieColonne());
+			System.out.println(Messages.afficherMessageTour(joueurActuel));
+			
+			boolean celluleVide = false;
+			int[] saisieCellule  = new int[2]; //saisieCellule[0] = Ligne et saisieCellule[1] = Colonne
+
+			while (!celluleVide) {
+				saisieCellule = Messages.saisirCellule(grille);
+				System.out.println(Messages.afficherMessageCellule(joueurActuel, saisieCellule));
+				if (grille.estVideCellule(saisieCellule[0], saisieCellule[1]))
+					celluleVide = true ;
+				else
+					System.out.println("La case selectionnee est pleine. Veuillez recommencer.\n");
+				}
+			grille.placerJeton(joueurActuel.getJeton(), saisieCellule[0], saisieCellule[1]);
+			
 			grille.afficherGrille();
-			if (MouvementsTicTacToe.alignementCellule(grille, interf.getSaisieLigne(), interf.getSaisieColonne(), 3) >=1 ) {
+			if (MouvementsTicTacToe.alignementCellule(grille,saisieCellule[0], saisieCellule[1], 3) >=1 ) {
 				joueurActuel.marquerPoint();
 				match.setVictoire(joueur1, joueur2);
 			}
-			interf.afficherMessageResultat(match, joueurActuel);
+			System.out.println(Messages.afficherMessageResultat(match, joueurActuel));
 		}
 	}
 
--- OOP-TicTacToe/src/oop/tictactoe/jouer/Messages.java
+++ OOP-TicTacToe/src/oop/tictactoe/jouer/Messages.java
@@ -5,105 +5,17 @@ import oop.tictactoe.jouer.Joueur;
 import oop.tictactoe.grille.*;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class Messages {
 	
 	/**
 	 * saisieCellule input de l utilisateur sous format de table à 1 colonne 2 lignes
 	 * messageResultat victoire ou non (càd "Continuez la partie!" ou "Le Joueur X a gagné la partie")
-	 * messageTour quel est le tour et qui doit jouer
+	 * messageTour indique qui doit jouer
+	 * messagePermutation quel cellule de la grille ont été permutées
 	 */
-	private int[] saisieCellule; //saisieCellule[0] : ligne, saisieCellule[1] : colonne
-	private String messageCellule ;
-	private String messageResultat ;
-	private String messageTour ;
 	
-	public Messages() {
-		messageCellule = "";
-		messageResultat = "";
-		messageTour = "" ;
-		saisieCellule = new int[2];
-	}
-	
-	/**
-	 * saisieCellule sous forme de int[]
-	 * saisieCellule[0] : ligne
-	 * saisieCellule[1] : colonne
-	 * definie la cellule saisie par l'utilisateur
-	 * securise le choix de la cellule par l utilisateur l entrée de l utilisateur
-	 */
-	public void setSaisieCellule(Grille grille) {
-		boolean saisieCorrecte = false ;
-
-		while ( !saisieCorrecte) {
-			
-			System.out.println("Veuillez choisir une case,\nex: pour la case de la ligne 1 à la colonne 2, tapez : 1-2),\npuis appuyez sur \'Entree\'.\n");
-			Scanner scanner = new Scanner(System.in);			
-			String[] saisie = scanner.nextLine().trim().split("-");
-			
-			try{
-				if(saisie.length==2) {
-					saisieCellule[0] = Integer.parseInt(saisie[0].trim())-1;
-					saisieCellule[1] = Integer.parseInt(saisie[1].trim())-1;
-					if (saisieCellule[0]>=0 && saisieCellule[0]<grille.getLignes()
-							&& saisieCellule[1]>=0 && saisieCellule[1]<grille.getColonnes()) {
-						if (grille.estVideCellule(saisieCellule[0], saisieCellule[1]))
-							saisieCorrecte = true ;
-						else
-							System.out.println("La case selectionnee est pleine");
-						}
-					else
-						System.out.println("Il faut choisir une ligne et une colonne de la grille.\n");
-					}
-				if(saisie.length!=2)
-					System.out.println("Saisie incorrecte, veuillez recommencer.\n");
-			}
-			catch(java.lang.NumberFormatException e1) {
-				System.out.println("Format invalide veuillez recommencer.\n");
-			}
-		}
-	}
-	
-	public void saisirCellule(Grille grille) {
-		boolean saisieCorrecte = false ;
-		
-		while ( !saisieCorrecte) {
-			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Veuillez choisir une case,\nex: pour la case de la ligne 1 à la colonne 2, tapez : 1-2),\npuis appuyez sur \'Entree\'.\n");
-			String saisie = sc.nextLine();
-			System.out.println("Vous avez saisi : " + saisie);
-			
-			try {
-				if (estValideSaisie(saisie)) {
-					int[] cellule = extraitCelluleSaisie(saisie);
-					
-					if (cellule[0]>=0 && cellule[0]<grille.getLignes()
-							&& cellule[1]>=0 && cellule[1]<grille.getColonnes()) {
-						saisieCorrecte = true ;
-
-//						if (grille.estVideCellule(cellule[0], cellule[1])) {
-//							saisieCellule[0] =  cellule[0];
-//							saisieCellule[1] =  cellule[1];
-//							}
-//						else
-//							System.out.println("La case selectionnee est pleine, veuillez recommencer.\n");
-						}
-					else
-						System.out.println("Il faut choisir une ligne et une colonne de la grille.\n");
-				}
-				else
-					System.out.println("Saisie incorrecte, veuillez recommencer.\n");
-			}
-			
-			catch(java.lang.NumberFormatException e1) {
-				System.out.println("Format invalide veuillez recommencer.\n");
-			}
-		}
-	}
-		
+//	PROF VERSION		
 //	public static void analyseSaisie(String s) {
 //		if (s.matches("\\s*\\d+\\s*-\\s*\\d+\\s*")) {
 //			Scanner sc = new Scanner(s);
@@ -117,7 +29,52 @@ public class Messages {
 //			System.out.println("Coup invalide");				
 //	}
 	
+	/**
+	 * saisieCellule sous forme de int[]
+	 * saisieCellule[0] : ligne
+	 * saisieCellule[1] : colonne
+	 * definie la cellule saisie par l'utilisateur
+	 * securise le choix de la cellule par l utilisateur l entrée de l utilisateur
+	 * ne verifie pas que la cellule est pleine ou non
+	 */
+	public static int[] saisirCellule(Grille grille) {
+		boolean saisieCorrecte = false ;
+		int[] cellule  = new int[2];
 
+		while ( !saisieCorrecte) {
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Veuillez choisir une case,\nex: pour la case de la ligne 1 a la colonne 2, tapez :\n1-2\npuis appuyez sur \'Entree\'.\n");
+			String saisie = sc.nextLine();
+			System.out.println("Vous avez tape : " + saisie +".\n");
+			
+			try {
+				if (estValideSaisie(saisie)) {
+					cellule = extraitCelluleSaisie(saisie);
+					//il faut tout de meme REverifier que la dimension est de 2
+					if (cellule.length == 2) {
+						//decrementation pour s adapter à l affichage
+						--cellule[0];
+						--cellule[1];
+						if (cellule[0]>=0 && cellule[0]<grille.getLignes()
+								&& cellule[1]>=0 && cellule[1]<grille.getColonnes()) {
+							//sortie de la boucle
+							saisieCorrecte = true ;
+							}
+						else
+							System.out.println("Il faut choisir une ligne et une colonne de la grille, recommencez.\n");
+					}
+				}
+				else
+					System.out.println("La saisie est incorrecte, recommencez.\n");
+			}
+			
+			catch(java.lang.NumberFormatException e1) {
+				System.out.println("Le format est invalide, recommencez.\n");
+			}
+		}
+		return cellule;
+	}	
 	
 	public static boolean estValideSaisie(String saisie) {
 		// format : entier[espaces]-[espaces]entier (ligne puis colonne)
@@ -125,11 +82,7 @@ public class Messages {
 		// \d 	Matches the digits. Equivalent to [0-9].
 		// re* 	Matches 0 or more occurrences of the preceding expression (e)
 		// re+  Matches 1 or more of the previous thing (e).
-		
-		if (saisie.matches("\\s*\\d+\\s*-\\s*\\d+\\s*"))
-			return true;
-		else
-			return false;
+		return saisie.matches("\\s*\\d+\\s*-\\s*\\d+\\s*");
 	}
 	
 	
@@ -144,38 +97,18 @@ public class Messages {
 		return coordonnees;
 	}
 	
-
-	public int[] getSaisieCellule() {
-		assert(saisieCellule != null) ; //on s assure que setSaisieCellule a ete appelee
-		return saisieCellule ;
-	}
-	
-	public int getSaisieColonne() {
-		assert(saisieCellule != null) ; //on s assure que setSaisieCellule a ete appelee
-		return saisieCellule[1] ;
-	}
-	public int getSaisieLigne() {
-		assert(saisieCellule != null) ; //on s assure que setSaisieCellule a ete appelee
-		return saisieCellule[0] ;
-	}
-	
 	
 	/**
-	 * messageCellule sous forme string
-	 * doit etre appele à la suite de setSaisieCellule()
-	 * @return L’utilisateur X (resp O) a saisie la cellule «[i],[j]»
+	 * messageCellule sous forme string indiquant ce que le joueur a selectionne comme cellule
+	 * doit etre appele à la suite de saisirCellule()
+	 * @return L’utilisateur X (resp O) a saisie la cellule ligne [i], colonne[j].
 	 */
-	public String getMessageCellule(Joueur j) {
+	public static String afficherMessageCellule(Joueur j, int[] celluleCoord) {
 		assert(j != null);
-		assert(saisieCellule != null) ; //on s assure que setSaisieCellule a ete appelee
-		int saisieLigne = getSaisieLigne()+1;
-		int saisieColonne = getSaisieColonne()+1;		
-		messageCellule = "L’utilisateur " + j.getJeton().getSymbole()+ " a saisie la cellule ligne ["+ saisieLigne +"], colonne ["+ saisieColonne  +"]." ;
-		return  messageCellule;
-	}
-	public void afficherMessageCellule(Joueur j) {
-		assert(j != null);
-		System.out.println(getMessageCellule(j));
+		assert(celluleCoord != null) ; //on s assure que setSaisieCellule a ete appelee
+		int saisieLigne = celluleCoord[0]+1;
+		int saisieColonne = celluleCoord[1]+1;		
+		return "L’utilisateur " + j.getJeton().getSymbole()+ " a choisi la cellule ligne ["+ saisieLigne +"], colonne ["+ saisieColonne  +"].\n" ;
 	}
 
 	/**
@@ -187,22 +120,19 @@ public class Messages {
 	 * @param j joueur qui vient de jouer
 	 * @return
 	 */
-	public String getMessageResultat(Match m, Joueur j) {
+	public static String afficherMessageResultat(Match m, Joueur j) {
 		assert(j != null && m != null);
+		String messageResultat;
 		if (m.estVictoire()) {
-			messageResultat = "C est un match victorieux pour le joueur "+j.getJeton().getSymbole()+".";
+			messageResultat = "C est un match victorieux pour le joueur "+j.getJeton().getSymbole()+".\n";
 		}
 		else {
 			if (m.estMatchNul())
-				messageResultat = "C est un match nul." ;
+				messageResultat = "C est un match nul.\n" ;
 			else
-				messageResultat = "Le joueur "+j.getJeton().getSymbole() + " a termine son tour.";
+				messageResultat = "Le joueur "+j.getJeton().getSymbole() + " a termine son tour.\n";
 		}
 		return messageResultat;
-	}
-	public void afficherMessageResultat(Match m, Joueur j) {
-		assert(j != null && m != null);
-		System.out.println(getMessageResultat(m,j));
 	}
 	
 	/**
@@ -210,14 +140,25 @@ public class Messages {
 	 * @param j joueur dont le tour commence
 	 * @return
 	 */
-	public String getMessageTour(Joueur j) {
+	public static String afficherMessageTour(Joueur j) {
 		assert(j != null);
-		messageTour = "C'est a l'utilisateur " + j.getJeton().getSymbole() + " de jouer.";
-		return messageTour ;
-	}
-	public void afficherMessageTour(Joueur j) {
-		assert(j != null);
-		System.out.println(getMessageTour(j));
+		return "C'est a l'utilisateur " + j.getJeton().getSymbole() + " de jouer.\n";
 	}
 	
+	/**
+	 * MessagePermutation
+	 * @param j joueur ayant realiser la permutation
+	 * @param celluleCoord1 premier cellule
+	 * @param celluleCoord2 seconde cellule
+	 * @return "L'utilisateur a choisi de permuter la cellule ligne ... avec la cellule ... ."
+	 */
+	public static String afficherMessagePermutation(Joueur j, int[] celluleCoord1,  int[] celluleCoord2 ) {
+		assert(j != null);
+		assert(celluleCoord1 != null && celluleCoord2 != null) ; //on s assure que setSaisieCellule a ete appelee
+		int saisieLigne1 = celluleCoord1[0]+1;
+		int saisieColonne1 = celluleCoord1[1]+1;
+		int saisieLigne2 = celluleCoord2[0]+1;
+		int saisieColonne2 = celluleCoord2[1]+1;	
+		return "L’utilisateur " + j.getJeton().getSymbole()+ " a choisi de permuter la cellule ligne ["+ saisieLigne1 +"] colonne ["+ saisieColonne1  +"], avec la cellule ligne ["+ saisieLigne2 +"] colonne ["+ saisieColonne2  +"].\n" ;
+	}
 }
